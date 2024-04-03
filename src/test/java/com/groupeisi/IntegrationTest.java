1f81ae21856185d02052950c416dc98d64d6b199
papeseck <diabel1208@gmail.com>
2024-04-03 18:42:45 +0200

    Initial version of webser generated by generator-jhipster@8.2.1

---
+++ src/test/java/com/groupeisi/IntegrationTest.java
@@ -0,0 +1,22 @@
+package com.groupeisi;
+
+import com.groupeisi.config.AsyncSyncConfiguration;
+import com.groupeisi.config.EmbeddedSQL;
+import com.groupeisi.config.JacksonConfiguration;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = { WebserApp.class, JacksonConfiguration.class, AsyncSyncConfiguration.class })
+@EmbeddedSQL
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+public @interface IntegrationTest {
+}
